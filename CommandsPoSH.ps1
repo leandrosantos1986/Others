679421d38659981d529d52ad431334bd80cfa960
Leandro Santos <leandro.santos1986@gmail.com>
2023-01-16 13:26:47 -0300

    Removed LFS

--- CommandsPoSH.ps1
+++ CommandsPoSH.ps1
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcc7d79d875db299e82e105f1e27dd5d8409f778858972c67036795848b59462
-size 11986
+oid sha256:2ca69f3207da33cb671f068221d5cae21facaca93fd48a0dbe10611dd578758c
+size 11994
